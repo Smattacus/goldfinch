bcedf2d7aff8aa67e09f519252f184766e6275e6
Smattacus <Smattacus@gmail.com>
2022-08-13 03:05:09 +0000

    Create a container with systemc based off of the vscode python3 container.

---
+++ containers/systemc:python3-bullseye/install_systemc.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+export SYSTEMC_VERSION=2.3.3
+export SYSTEMC_AMS_VERSION=2.3
+export CXX=g++
+export SYSTEMC_HOME=/usr/local/systemc-$SYSTEMC_VERSION
+export LD_LIBRARY_PATH=/usr/local/systemc-$SYSTEMC_VERSION/lib-linux64
+
+cd /usr/local
+# Install systemc
+tar -xzf systemc-${SYSTEMC_VERSION}.tar.gz
+
+mkdir systemc-${SYSTEMC_VERSION}/objdir
+
+cd systemc-${SYSTEMC_VERSION}/objdir
+../configure --prefix=/usr/local/systemc-$SYSTEMC_VERSION
+make
+make install
+cd ..
+rm -rf objdir
+
+# Install systemc-ams
+cd /usr/local
+tar -xzf systemc-ams-${SYSTEMC_AMS_VERSION}.tar.gz
+
+mkdir systemc-ams-${SYSTEMC_AMS_VERSION}/objdir
+
+cd systemc-ams-${SYSTEMC_AMS_VERSION}/objdir
+../configure --prefix=/usr/local/systemc-ams-${SYSTEMC_AMS_VERSION}
+make
+make install
+cd ..
+rm -rf objdir
