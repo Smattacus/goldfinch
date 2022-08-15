752d2ca4c2c07e18f9bb72d7119ea097b257e743
Smattacus <Smattacus@gmail.com>
2022-08-15 16:53:45 +0000

    Basic hello world from SystemC!

--- systemc/hello/hello.cpp
+++ systemc/hello/hello.cpp
@@ -6,8 +6,28 @@ void hello1()
     std::cout << "Hello world without systemc" << std::endl;
 }
 
+struct HelloWorld : sc_module
+{
+
+    SC_CTOR(HelloWorld)
+    {
+
+        SC_METHOD(hello2);
+    }
+
+    void hello2(void)
+    {
+        std::cout << "Hello world from SystemC!!" << std::endl;
+    }
+};
+
 int sc_main(int args, char *[])
 {
     hello1();
+    HelloWorld
+        helloworld("helloworld");
+
+    sc_start();
+
     return 0;
 }
